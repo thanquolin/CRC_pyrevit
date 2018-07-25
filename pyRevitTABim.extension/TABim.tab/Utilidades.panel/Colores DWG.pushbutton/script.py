957efc4f99caa0dd7e0849c5a6257766a3832b03
thanquolin <thanquolin@gmail.com>
2018-07-25 13:33:18 +0200

    Update script.py

--- pyRevitTABim.extension/TABim.tab/Utilidades.panel/Colores DWG.pushbutton/script.py
+++ pyRevitTABim.extension/TABim.tab/Utilidades.panel/Colores DWG.pushbutton/script.py
@@ -7,18 +7,18 @@
 __title__ = 'Aplicar Colores de\nPlantilla a DWG'
 __author__  = 'Carlos Romero Carballo'
 
+from pyrevit.coreutils import Timer
+timer = Timer()
 import csv
 import clr
 import os.path
 import Autodesk.Revit.DB as DB
 from Autodesk.Revit.DB import *
-from pyrevit.coreutils import Timer
 
-timer = Timer()
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
-plantilla_dwg_path = "C:\ProgramData\pyRevit\pyRevit-v45\pyRevit\extensions\pyRevitTABim.extension\TABim.tab\Colores Plantilla a DWG.panel\Colores DWG.pushbutton\dwg_to_rgb.csv"
-plantilla_capas_path = "C:\ProgramData\pyRevit\pyRevit-v45\pyRevit\extensions\pyRevitTABim.extension\TABim.tab\Colores Plantilla a DWG.panel\Colores DWG.pushbutton\plantilla_capas.csv"
+plantilla_dwg_path = "C:\ProgramData\pyRevit\pyRevit-v45\pyRevit\extensions\pyRevitTABim.extension\TABim.tab\Utilidades.panel\Colores DWG.pushbutton\dwg_to_rgb.csv"
+plantilla_capas_path = "C:\ProgramData\pyRevit\pyRevit-v45\pyRevit\extensions\pyRevitTABim.extension\TABim.tab\Utilidades.panel\Colores DWG.pushbutton\plantilla_capas.csv"
 cads_categories = [cat for cat in doc.Settings.Categories if cat.Name.ToString().endswith(".dwg")]
 capas_dwg = [item for sublist in [cat.SubCategories for cat in doc.Settings.Categories if cat.Name.ToString().endswith(".dwg")] for item in sublist]
 
@@ -27,7 +27,8 @@ if len(capas_dwg) == 0:
 elif not os.path.exists(plantilla_dwg_path):
     print("No se encuentra 'dwg_to_rgb.csv' en el directorio del script.")
 elif not os.path.exists(plantilla_capas_path):
-    print("No se encuentra 'plantilla_capas.csv' en el directorio del script.\nPara abrir el directorio, hacer click en el botón del script mientras se pulsa la tecla ALT.\nMás información en el tooltip del script.")
+    print("No se encuentra 'plantilla_capas.csv' en el directorio del script.\nPara abrir el directorio,\
+     hacer click en el botón del script mientras se pulsa la tecla ALT.\nMás información en el tooltip del script.")
 
 else:
     plantilla_dwg = {}
@@ -56,7 +57,8 @@ else:
             capa.LineColor = plantilla_colores[capa.Name]
             print("Color de Capa '{}' cambiado en '{}'.".format(capa.Name,capa.Parent.Name))
     #Movemos los cad para que se recarguen, mostrando los cambios en color.
-    for cad in [item for sublist in [FilteredElementCollector(doc).OfCategoryId(cat.Id).WhereElementIsNotElementType().ToElements() for cat in doc.Settings.Categories if cat.Name.ToString()[-4:] == ".dwg"] for item in sublist]:
+    for cad in [item for sublist in [FilteredElementCollector(doc).OfCategoryId(cat.Id).WhereElementIsNotElementType().ToElements()\
+     for cat in doc.Settings.Categories if cat.Name.ToString()[-4:] == ".dwg"] for item in sublist]:
         if cad.Pinned == False:
             ElementTransformUtils.MoveElement(doc, cad.Id, XYZ.BasisX)
             ElementTransformUtils.MoveElement(doc, cad.Id, XYZ.BasisX.Negate())
