554c6703adc5152074aa853721d5f27626bed4c6
thanquolin <thanquolin@gmail.com>
2018-09-12 13:21:33 +0200

    Update memoria_carpinteria.py

--- scripts/WIP/memoria_carpinteria.py
+++ scripts/WIP/memoria_carpinteria.py
@@ -1,5 +1,5 @@
 # coding: utf8
-"""Crea un alzado recortado para cada tipo de puerta y ventana en el proyecto, desde el exterior, con el nombre del tipo. Debe existir alguna vista 3D en el proyecto."""
+"""Crea una sección para cada tipo de puerta y ventana en el proyecto, desde el exterior, con el nombre del tipo, y alberga estas secciones en la Sheet 999-Memoria Carpintería."""
 
 #pyRevit info
 __title__ = 'Memoria\nCarpintería'
@@ -37,16 +37,17 @@ instances = door_types_ex + window_types_ex
 
 
 def inst_width(instance):
-    valid = ["Width", "Anchura"] #TEMA IDIOMA! USA ESTE MÉTODO LOS PARÁMETROS DE LA API?
     if len(instance.GetParameters("Width")) == 1:
         return instance.GetParameters("Width")[0].AsDouble()
-    elif len(instance.GetParameters("Width")) != 1:
-        if len(doc.GetElement(instance.GetTypeId()).GetParameters("Width")) == 1:
-            return doc.GetElement(instance.GetTypeId()).GetParameters("Width")[0].AsDouble()
-        elif len(doc.GetElement(instance.GetTypeId()).GetParameters("Width")) > 1:
-            return doc.GetElement(instance.GetTypeId()).GetParameters("Width")[0].AsDouble()
-        else:
-            return 6.56168
+    else:
+        return doc.GetElement(instance.GetTypeId()).GetParameters("Width")[0].AsDouble()
+
+def inst_height(instance):
+    if len(instance.GetParameters("Height")) == 1:
+        return instance.GetParameters("Height")[0].AsDouble()
+    else:
+        return doc.GetElement(instance.GetTypeId()).GetParameters("Height")[0].AsDouble()
+
 
 mag = list()
 
@@ -56,20 +57,19 @@ for element in instances:
     minZ = bb.Min.Z
     maxZ = bb.Max.Z
     width = inst_width(element)
-    height = maxZ - minZ
-    distance = 4
     offset = 1
-
-    min = XYZ( -width/2 - offset, minZ - offset, -offset) if element.Category.Name == "Doors" else XYZ( -width/2 - offset, minZ - offset*2, -offset)
-    max = XYZ( width/2 + offset, maxZ + offset, offset) if element.Category.Name == "Doors" else XYZ( width/2 + offset, maxZ, offset)
-
+    height = inst_height(element)
+    min = XYZ( -width/2 - offset, -(height/2 + element.Location.Point.Z - doc.GetElement(element.LevelId).Elevation) - offset , -offset)
+    max = XYZ( width/2 + offset, (height)/2 + offset , offset)
+    section_height = - min.Y + max.Y
+    section_width = width + 2*offset
     midpoint = element.Location.Point if element.Location != None else XYZ(0,0,0)
     normal = element.FacingOrientation
     up = XYZ.BasisZ
     viewdir = normal.Negate()
 
     transform = Transform.Identity
-    transform.Origin = midpoint
+    transform.Origin = XYZ(midpoint.X, midpoint.Y, height/2 + element.Location.Point.Z)
     transform.BasisX = normal.CrossProduct(up)
     transform.BasisY = up
     transform.BasisZ = viewdir
@@ -78,8 +78,7 @@ for element in instances:
     new_bb.Transform = transform
     new_bb.Min = min
     new_bb.Max = max
-    mag.append([new_bb, "MC-" + element.Symbol.Family.Name + "-" + element.Name, element.Id])
-
+    mag.append([new_bb, "MC-" + element.Symbol.Family.Name + "-" + element.Name, element.Id, section_height, section_width])
 
 t = Transaction(doc,"Vistas Memoria Carpintería")
 t.Start()
@@ -94,16 +93,19 @@ for bb in mag:
     section = ViewSection.CreateSection(doc, vft, bb[0])
     section.GetParameters("View Name")[0].Set(bb[1])
     section.ChangeTypeId(mc.Id)
-    section.Scale = 10
-    new_views.append([section, bb[2]])
+    scale = 10
+    section.Scale = scale
+    new_views.append([section, bb[2], bb[3], bb[4]])
 invalid_el_id = ElementId.InvalidElementId
 sheet = ViewSheet.Create(doc, invalid_el_id)
 sheet.Name = "Memoria Carpintería"
 sheet.SheetNumber = "A999"
 coord = 0
+prev = 0
 for view in new_views:
-    Viewport.Create(doc, sheet.Id, view[0].Id, XYZ(coord,0,0))
-    coord += 0.5
+    coord = prev + offset + view[3]
+    Viewport.Create(doc, sheet.Id, view[0].Id, XYZ(coord/scale,view[2]/scale/2,0))
+    prev = coord
 t.Commit()
 
 
