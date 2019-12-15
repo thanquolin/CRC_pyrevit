5580fbc47122863cb30e592e276914dbe70b6de2
Carlos Romero <thanquolin@gmail.com>
2019-12-15 22:50:16 +0100

    Update notas.py

--- notas.py
+++ notas.py
@@ -1,17 +1,15 @@
 import clr
+
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
 
 clr.AddReference('RevitAPI')
-import Autodesk
 from Autodesk.Revit.DB import *
 
 clr.AddReference('DSCoreNodes')
-import DSCore
 from DSCore import *
 
 clr.AddReference('RevitServices')
-import RevitServices
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
@@ -19,7 +17,10 @@ doc = DocumentManager.Instance.CurrentDBDocument
 
 
 #Casting en python para sólidos (no podemos usar "as").
+
 solids = [solid for solid in el.get_Geometry(Options())]
 
-#Para utilizar un overload concreto de un método de la api, hay que especificarlo en el índice.
-#Elemento.Método.Overloads.Functions[index](resto de variables)
+#Para utilizar un overload concreto de un método de la api, especificando índice del overload.
+#Elemento.Método.Overloads.Functions[índice](resto de variables)
+
+solid_A.Intersect.Overloads.Functions[2](solid_B)
